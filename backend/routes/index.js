c1e9a6d0d702237894aab0cccfaf9dc1bfff36db
binoyskariainferq <binoy@inferq.com>
2025-03-19 18:15:27 +0530

    add post feature added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const helloRoutes = require('./helloRoutes');
+const postsRoutes = require('./posts');
 
 const router = express.Router();
 
 router.use('/hello', helloRoutes);
+router.use('/posts', postsRoutes);
 
 module.exports = router;
