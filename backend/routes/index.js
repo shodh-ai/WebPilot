2dd38afbb4187397321fd985127a6cdcd357ddf6
binoyskariainferq <binoy@inferq.com>
2025-03-19 19:36:23 +0530

    send message feature added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const helloRoutes = require('./helloRoutes');
+const messagesRoutes = require('./messages');
 
 const router = express.Router();
 
 router.use('/hello', helloRoutes);
+router.use('/messages', messagesRoutes);
 
 module.exports = router;
