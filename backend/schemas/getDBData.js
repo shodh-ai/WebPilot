a3790bb83aedb49c7d165203aa6e73ecd81959c8
Harshit Sandilya <harshitsandilya@gmail.com>
2025-03-25 00:57:02 +0530

    Generalised table

--- backend/schemas/getDBData.js
+++ backend/schemas/getDBData.js
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
 export const getDBData = z.object({
-  tables: z.string().min(1, { message: 'Tables cannot be empty' }),
+  user_id: z.string().min(1, { message: 'User ID cannot be empty' }),
+  tables: z.array(z.string().min(1, { message: 'Tables cannot be empty' })),
+  columns: z.array(z.array(z.string().min(1, { message: 'Columns cannot be empty' })).optional())
 })
