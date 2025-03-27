c773ad0929b2cc50787c74ac6f20775140facd79
binoyskariainferq <binoy@inferq.com>
2025-03-27 12:03:10 +0530

    fixes : made getDBData compatible with auth

--- backend/schemas/getDBData.js
+++ backend/schemas/getDBData.js
@@ -1,7 +1,6 @@
 import { z } from 'zod';
 
 export const getDBData = z.object({
-  user_id: z.string().min(1, { message: 'User ID cannot be empty' }),
   tables: z.array(z.string().min(1, { message: 'Tables cannot be empty' })),
   columns: z.array(z.array(z.string().min(1, { message: 'Columns cannot be empty' })).optional())
 })
